e6d661ef216f4951cf2b269b4f2e266c1c35e9db
Jonathan F. K <jonathanfk1@gmail.com>
2021-07-28 19:29:57 -0300

    Fixing parallax.

--- Levels/GameOver.gd
+++ Levels/GameOver.gd
@@ -1,9 +1,9 @@
 extends Control
 
 func _on_RectangleButton_pressed():
-	get_tree().change_scene("res://Levels/Level0.tscn")
+	get_tree().change_scene("res://Levels/Level1.tscn")
 
 func _input(event):
 	if event.is_action_pressed('Jump') or event.is_action_pressed('Ok'):
-		get_tree().change_scene("res://Levels/Level0.tscn")
+		get_tree().change_scene("res://Levels/Level1.tscn")
 	
