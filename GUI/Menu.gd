5c80c5eebf126e1261df2cddd17e41b279e003fc
jayypluss <jonathanfk1@gmail.com>
2021-07-27 23:52:01 -0300

    Refactoring pause and adding credits to menu.

---
+++ GUI/Menu.gd
@@ -0,0 +1,21 @@
+extends ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func pause():
+	get_tree().paused = not get_tree().paused
+	$Pause.visible = get_tree().paused
+	visible = get_tree().paused
+	if visible:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		pause()
+
+func _on_ResumeButton_pressed():
+	pause()
--- GUI/Pause.gd
+++ GUI/Pause.gd
@@ -9,17 +9,3 @@ func _ready():
 #  $VBoxContainer/MusicVolumeLabel.text = music_volume_text % Configuration.get_value("audio", "music_volume")
 #  $VBoxContainer/MusicVolumeSlider.value = Configuration.get_value("audio", "music_volume")
 
-func pause():
-	get_tree().paused = not get_tree().paused
-	visible = get_tree().paused
-	if visible:
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	else:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _input(event):
-	if event.is_action_pressed("Pause"):
-		pause()
-
-func _on_ResumeButton_pressed():
-	pause()
